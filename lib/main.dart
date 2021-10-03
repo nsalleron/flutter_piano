326d791243a7c4efb8818be4f7a6bdcabe7a115b
Nicolas Salleron <nicolas.salleron@gmail.com>
2021-10-03 19:09:08 +0200

    you can play with keyboard now :o)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(FlutterPiano());
@@ -21,7 +24,6 @@ class FlutterPiano extends StatelessWidget {
 }
 
 class FlutterPianoScreen extends StatefulWidget {
-
   final String title = 'Flutter Piano';
 
   @override
@@ -30,11 +32,44 @@ class FlutterPianoScreen extends StatefulWidget {
 
 class _FlutterPianoScreenState extends State<FlutterPianoScreen> {
   final assetsAudioPlayer = AssetsAudioPlayer();
+  final FocusNode _focusNode = FocusNode();
 
   void _readTone(String tone) {
     assetsAudioPlayer.open(
       Audio("assets/notes/$tone.mp3"),
     );
+  }
+
+  void _handleKeyEvent(RawKeyEvent event) {
+    if (event.logicalKey == LogicalKeyboardKey.keyZ)
+      _readTone('C');
+    else if (event.logicalKey == LogicalKeyboardKey.keyX)
+      _readTone('D');
+    else if (event.logicalKey == LogicalKeyboardKey.keyC)
+      _readTone('E');
+    else if (event.logicalKey == LogicalKeyboardKey.keyV)
+      _readTone('F');
+    else if (event.logicalKey == LogicalKeyboardKey.keyB)
+      _readTone('G');
+    else if (event.logicalKey == LogicalKeyboardKey.keyN)
+      _readTone('A');
+    else if (event.logicalKey == LogicalKeyboardKey.keyM)
+      _readTone('B');
+    else if (event.logicalKey == LogicalKeyboardKey.keyS)
+      _readTone('Db');
+    else if (event.logicalKey == LogicalKeyboardKey.keyD)
+      _readTone('Eb');
+    else if (event.logicalKey == LogicalKeyboardKey.keyG)
+      _readTone('Gb');
+    else if (event.logicalKey == LogicalKeyboardKey.keyH)
+      _readTone('Ab');
+    else if (event.logicalKey == LogicalKeyboardKey.keyJ) _readTone('Bb');
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
   }
 
   @override
@@ -47,22 +82,27 @@ class _FlutterPianoScreenState extends State<FlutterPianoScreen> {
         child: LayoutBuilder(
           builder: (BuildContext context, BoxConstraints constraints) {
             double whiteWidth = constraints.maxWidth / 7;
-            return Stack(
-              fit: StackFit.expand,
-              children: [
-                whiteTile('C', 0, whiteWidth),
-                whiteTile('D', 1, whiteWidth),
-                whiteTile('E', 2, whiteWidth),
-                whiteTile('F', 3, whiteWidth),
-                whiteTile('G', 4, whiteWidth),
-                whiteTile('A', 5, whiteWidth),
-                whiteTile('B', 6, whiteWidth),
-                blackTile('Db', 1 , whiteWidth, constraints.maxHeight / 2),
-                blackTile('Eb', 2 , whiteWidth, constraints.maxHeight / 2),
-                blackTile('Gb', 4 , whiteWidth, constraints.maxHeight / 2),
-                blackTile('Ab', 5 , whiteWidth, constraints.maxHeight / 2),
-                blackTile('Bb', 6 , whiteWidth, constraints.maxHeight / 2),
-              ],
+            return RawKeyboardListener(
+              autofocus: true,
+              focusNode: _focusNode,
+              onKey: _handleKeyEvent,
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  whiteTile('C', 0, whiteWidth),
+                  whiteTile('D', 1, whiteWidth),
+                  whiteTile('E', 2, whiteWidth),
+                  whiteTile('F', 3, whiteWidth),
+                  whiteTile('G', 4, whiteWidth),
+                  whiteTile('A', 5, whiteWidth),
+                  whiteTile('B', 6, whiteWidth),
+                  blackTile('Db', 1, whiteWidth, constraints.maxHeight / 2),
+                  blackTile('Eb', 2, whiteWidth, constraints.maxHeight / 2),
+                  blackTile('Gb', 4, whiteWidth, constraints.maxHeight / 2),
+                  blackTile('Ab', 5, whiteWidth, constraints.maxHeight / 2),
+                  blackTile('Bb', 6, whiteWidth, constraints.maxHeight / 2),
+                ],
+              ),
             );
           },
         ),
@@ -88,7 +128,8 @@ class _FlutterPianoScreenState extends State<FlutterPianoScreen> {
     );
   }
 
-  Widget blackTile(String tone, double position, double whiteWidth, double height) {
+  Widget blackTile(String tone, double position, double whiteWidth,
+      double height) {
     double blackWidth = whiteWidth * 0.60;
     return Positioned(
       top: 0,
